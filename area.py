16bdfbd153a87776a3d1ef42335a7df0c84ca953
Bindhushree <miss.bindhushree7676@gmail.com>
2026-03-26 10:35:38 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,12 @@
+##rad=float(input("enter the radii of a circle"))
+##area=(22/7)*(rad**2)
+##peri=2*(22/7)*rad
+##print(f"area{area},perimeter{peri}")
+##rad=float(input("enter the radii of a circle"))
+##area=(22/7)*(rad**2)
+##peri=2*(22/7)*rad
+##print(f"area{area:.0f},perimeter{peri:.0f}")
+rad=float(input("enter the radii of a circle"))
+area=(22/7)*(rad**2)
+peri=2*(22/7)*rad
+print(f"area{area:.2f},perimeter{peri:.2f}")
